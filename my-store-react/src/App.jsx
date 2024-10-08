83bee7cbbd66173c6555008ff8603f5923e692c8
Umar <omarhafeez786@gmail.com>
2024-10-08 16:38:22 +0500

    react setup on another server

---
+++ my-store-react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-700">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
